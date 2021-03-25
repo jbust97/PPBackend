8a73a866afbb3de3ac1e91736646d22b06f342ca
fascmax <ivamax14@gmail.com>
2021-03-25 16:39:06 -0300

    Agregado el modelo de concepto de uso de puntos.

---
+++ src/main/java/py/com/progweb/prueba/model/ConceptoUsoPuntos.java
@@ -0,0 +1,51 @@
+package py.com.progweb.prueba.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="concepto_uso_puntos")
+public class ConceptoUsoPuntos {
+
+    @Id
+    @Column(name="id_concepto")
+    @Basic(optional=false)
+    @GeneratedValue(generator="conceptoSec", strategy=GenerationType.SEQUENCE)
+    @SequenceGenerator(name="conceptoSec", sequenceName="concepto_sec", allocationSize=0)
+    private Integer idConcepto;
+
+    @Column(name="descripcion", length=50)
+    @Basic(optional=false)
+    private String descripcion;
+
+    @Column(name="puntos_requeridos")
+    @Basic(optional=false)
+    private Integer puntosRequeridos;
+
+    public ConceptoUsoPuntos(){}
+
+    public Integer getIdConcepto() {
+        return idConcepto;
+    }
+
+    public void setIdConcepto(Integer idConcepto) {
+        this.idConcepto = idConcepto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getPuntosRequeridos() {
+        return puntosRequeridos;
+    }
+
+    public void setPuntosRequeridos(Integer puntosRequeridos) {
+        this.puntosRequeridos = puntosRequeridos;
+    }
+
+}
