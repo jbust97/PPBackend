18f4667f7d2fb89f237301f0e166944a79da0c86
jbus97 <hugofleitasbus@gmail.com>
2021-03-28 15:27:38 -0400

    agrega consulta sobre uso de puntos

--- src/main/java/py/com/progweb/prueba/rest/ConsultaRest.java
+++ src/main/java/py/com/progweb/prueba/rest/ConsultaRest.java
@@ -4,10 +4,14 @@ package py.com.progweb.prueba.rest;
 
 import py.com.progweb.prueba.ejb.BolsaPuntosDAO;
 import py.com.progweb.prueba.ejb.PersonaDAO;
+import py.com.progweb.prueba.ejb.UsoPuntosDAO;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Path("consulta")
 @Consumes("Application/json")
@@ -15,14 +19,24 @@ import javax.ws.rs.core.Response;
 public class ConsultaRest {
 
     @Inject
-    private PersonaDAO personaDAO;
-    @Inject
     private BolsaPuntosDAO bolsaPuntosDAO;
+    @Inject
+    private UsoPuntosDAO usoPuntosDAO;
+
     @GET
     @Path("/bolsa")
     public Response consultaBolsa(@QueryParam("id_persona") Integer idPersona,
                                   @QueryParam("limite_inferior") Integer limiteInferior,
                                   @QueryParam("limite_superior") Integer limiteSuperior){
         return Response.ok(bolsaPuntosDAO.getListaFiltrada(idPersona,limiteInferior,limiteSuperior)).build();
+    }
+
+    @GET
+    @Path("/uso-punto")
+    public Response consultaUso(@QueryParam("id_concepto") Integer idConcepto,
+                                  @QueryParam("fecha") String fechaUso,
+                                  @QueryParam("id_persona") Integer idPersona){
+
+        return Response.ok(usoPuntosDAO.getListaFiltrada(idConcepto,fechaUso,idPersona)).build();
     }
 }
